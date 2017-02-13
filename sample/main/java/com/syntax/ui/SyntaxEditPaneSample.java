a5d8881a2467db179d95434561b7c3a74cda540b
w86763777 <w86763777@gmail.com>
2017-02-13 13:40:55 +0800

    modify build.gradle

---
+++ sample/main/java/com/syntax/ui/SyntaxEditPaneSample.java
@@ -0,0 +1,12 @@
+package com.syntax.ui;
+
+import javax.swing.JFrame;
+
+public class SyntaxEditPaneSample {
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("SyntaxEditPaneSample");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700, 500);
+        frame.setVisible(true);
+    }
+}
